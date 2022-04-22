bd7338e38000c6862a3dba046d3f1756115c8e36
shahida191 <102863814+shahida191@users.noreply.github.com>
2022-04-22 09:11:55 -0700

    Add files via upload

---
+++ phonebook-project-main/contact.php
@@ -0,0 +1,24 @@
+
+<html>
+<head>
+<title>Phone Book</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id = "main">
+  <h1> Phone Book</h1>
+  <?php 
+  
+  include_once 'menu-main.php';
+  ?>
+  
+<p class="cpara"> You can contact me without any hesitation.</p>
+<h2 class="chead">Personal cell no</h2>
+<p class="cpara">01939020113</p> 
+ <h2 class="chead">Whatsapp no</h2>
+<p class="cpara">01939020113</p> 
+  <h2 class="chead">You can directly contact me via email</h2>
+<p class="cpara">tajul15-2645@diu.edu.bd</p>
+</div>
+</body>
+</html>
